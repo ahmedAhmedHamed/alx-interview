693c35731e3d3a8582032d76b131c25f577867af
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-10-08 18:30:54 +0300

    (0)

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""solution for lockboxes"""
+
+
+def canUnlockAll(boxes):
+    """solution for lockboxes"""
+    keys = []
+    visited = set()
+    count = len(boxes)
+    if len(boxes) <= 1:
+        return True
+    if len(boxes[0]) == 0:
+        return False
+    for key in boxes[0]:
+        keys.append(key)
+    while keys:
+        current = keys.pop()
+        if current < len(boxes) and current not in visited:
+            visited.add(current)
+            count -= 1
+            for key in boxes[current]:
+                keys.append(key)
+        if count == 0:
+            return True
+    if count == 0:
+        return True
+    return False
