00d415ffdd28d816aca639bd8df4d463280caa53
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2024-10-23 17:54:24 +0300

    while true loop done

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -25,7 +25,7 @@ test = r'238.31.240.119 - [2024-10-23 14:17:46.877469] "GET /projects/260 HTTP/1
 
 start = "(^"
 ip_regex = (r'([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.([01]?[0-9][0-9]?|2['
-r'0-4][0-9]|25[0-5])\.([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5]))')
+            r'0-4][0-9]|25[0-5])\.([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5]))')
 middle = r' - '
 year_regex = r'\[([0-9][0-9][0-9][0-9])-'
 month_regex = '(1[0-2]|[0-9])-'
@@ -45,7 +45,17 @@ after_status = after_request
 file_size_regex = '(([1-9][0-9]*)'
 end = '$)'
 complete = (start + ip_regex + middle + year_regex + month_regex
-+ day_regex + hour_regex + minute_regex + after_minute + second_regex + after_second + before_millisecond + millisecond_regex
-+ after_millisecond + request_regex + after_request + status_codes_regex + after_status
-+ file_size_regex + end)
-print()
+            + day_regex + hour_regex + minute_regex + after_minute + second_regex + after_second + before_millisecond + millisecond_regex
+            + after_millisecond + request_regex + after_request + status_codes_regex + after_status
+            + file_size_regex + end)
+counter = 0
+while True:
+    line = input()
+    x = re.match(complete, test)
+    start = x.start()
+    end = x.end()
+    counter += 1
+    if start != 0 or end != len(line):
+        print(line)
+        print(counter)
+        print()
