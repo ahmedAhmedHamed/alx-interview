7f41d67de4dfcd3283094f899a8366f63a321be2
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2024-10-23 14:45:42 +0300

    (0)

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -16,7 +16,36 @@ second: same as minute.
 millisecond: [0-9] 6 times
  "GET /projects/260 HTTP/1.1" :  "GET /projects/260 HTTP/1.1"
 status code: 200 | 301 | 400 | 401 | 403 | 404 | 405 | 500
-filesize: [1-9][0-9]*
+filesize: [0-9] * 6
 """
-import datetime
-print(datetime.datetime.now())
+# <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+import re
+
+test = r'238.31.240.119 - [2024-10-23 14:17:46.877469] "GET /projects/260 HTTP/1.1" 404 183'
+
+start = "(^"
+ip_regex = (r'([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.([01]?[0-9][0-9]?|2['
+r'0-4][0-9]|25[0-5])\.([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5]))')
+middle = r' - '
+year_regex = r'\[([0-9][0-9][0-9][0-9])-'
+month_regex = '(1[0-2]|[0-9])-'
+day_regex = '(2[0-3]|[0-1]?[0-9]) '
+hour_regex = r'(2[0-3]|[0-1]?[0-9]):'
+minute_regex = '([0-5]?[0-9])'
+after_minute = r':'
+second_regex = minute_regex
+after_second = r'\.'
+before_millisecond = r'('
+millisecond_regex = '[0-9]' * 6
+after_millisecond = r')\] '
+request_regex = r'"GET \/projects\/260 HTTP\/1.1"'
+after_request = ' '
+status_codes_regex = '(200|301|400|401|403|404|405|500)'
+after_status = after_request
+file_size_regex = '(([1-9][0-9]*)'
+end = '$)'
+complete = (start + ip_regex + middle + year_regex + month_regex
++ day_regex + hour_regex + minute_regex + after_minute + second_regex + after_second + before_millisecond + millisecond_regex
++ after_millisecond + request_regex + after_request + status_codes_regex + after_status
++ file_size_regex + end)
+print()
