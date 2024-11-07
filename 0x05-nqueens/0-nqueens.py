c7ce55f49289d5e7d269b4fdc2d1732d3e097db5
ahmedAhmedHamed <124739282+ahmedAhmedHamed@users.noreply.github.com>
2024-11-07 13:58:31 +0200

    (0)

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -13,6 +13,7 @@ import sys
 
 
 def find_ones_indices(arr):
+    """ as name implies, returns array of them [i, j] """
     indices = []
     for i, row in enumerate(arr):
         for j, value in enumerate(row):
@@ -26,21 +27,47 @@ def check_possible_move(board, row, col):
     check if diagonals have a queen
     then check if  rows have a queen
     then check if cols have a queen
+    up is row--
+    down is row++
+    left is col--
+    right is col++
     """
     n = len(board)
-    # check main diagonal
-    for i in range(len(board)):
-        if i + col >= len(board):
-            break
-        if board[i][i + col] == 1:
+    # check up and right
+    temp_row = row
+    temp_col = col
+    while temp_row >= 0 and temp_col < n:
+        if board[temp_row][temp_col] == 1:
             return False
+        temp_row -= 1
+        temp_col += 1
 
-    # check anti diagonal
-    for i in range(len(board)):
-        if col - i < 0:
-            break
-        if board[i][col - i] == 1:
+    # check down and left
+    temp_row = row
+    temp_col = col
+    while temp_row < n and temp_col >= 0:
+        if board[temp_row][temp_col] == 1:
             return False
+        temp_row += 1
+        temp_col -= 1
+
+    # down and right
+    temp_row = row
+    temp_col = col
+    while temp_row < n and temp_col < n:
+        if board[temp_row][temp_col] == 1:
+            return False
+        temp_row += 1
+        temp_col += 1
+
+    # up and left
+    temp_row = row
+    temp_col = col
+    while temp_row >= 0 and temp_col >= 0:
+        if board[temp_row][temp_col] == 1:
+            return False
+        temp_row -= 1
+        temp_col -= 1
 
     temp_row = 0
     while temp_row < len(board):
@@ -62,10 +89,7 @@ def nqueens(board, current_column=0):
     start from leftmost and go to the right
     """
     if current_column == len(board[0]):
-        # print(board)
-        for row in board:
-            print(row)
-        print("____")
+        print(find_ones_indices(board))
         return True
     for i in range(0, len(board)):
         if check_possible_move(board, i, current_column):
