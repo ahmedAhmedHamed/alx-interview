99f53ab602a462dec5f8a3e0ebbb1e16f13fb5fa
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-12-10 06:24:03 +0200

    0x0A solved, unoptimised

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+def get_count_primes(n: int) -> int:
+    """calculates number of primes using sieve."""
+    nums = [i for i in range(2, n + 1)]
+    count_primes = len(nums)
+    for num in nums:
+        if num == -1:
+            continue
+        mul = num
+        while num * mul <= n:
+            if nums[(num * mul) - 2] != -1:
+                count_primes -= 1
+                nums[(num * mul) - 2] = -1
+            mul += 1
+    return count_primes
+
+
+def isWinner(x, nums):
+    """
+    keep a count of each player's wins
+    loop over nums:
+        a player's win is decided by whether the number
+        of primes up to a num is odd or even.
+        get the number using the sieve of Eratosthenes
+    """
+    odd_wins = 0
+    even_wins = 0
+    for num in nums:
+        num_primes = get_count_primes(num)
+        if num_primes % 2:
+            odd_wins += 1
+        else:
+            even_wins += 1
+    return 'Maria' if odd_wins > even_wins else 'Ben'
